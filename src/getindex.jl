d791c629ebd8af176b3b75d0a125b8edb5f2dad0
David MacMahon <davidm@astro.berkeley.edu>
2021-09-14 22:26:29 +0000

    Iterate and getindex via Cprt rather than object
    Iterating a linked list is now done via the `Cptr` value and returns
    `Cptr` values rather than `c"struct ..."` objects.  Likewise, `getindex`
    now accepts `Cptr` values rather than `c"struct ..."` objects.  This
    simplifies these operations and keeping everything as pointers is
    (presumably) more efficient.

--- src/getindex.jl
+++ src/getindex.jl
@@ -1,21 +1,21 @@
 import Base: getindex
 
-# Index by `struct hashpipe_ibv_send_pkt`
-function Base.getindex(a::AbstractArray, sendpkt::c"struct hashpipe_ibv_send_pkt")
-    Base.getindex(a, sendpkt.wr.wr_id+1)
+# Index by `Cptr{c"struct hashpipe_ibv_send_pkt"}`
+function Base.getindex(a::AbstractArray, ppkt::Cptr{c"struct hashpipe_ibv_send_pkt"})
+    Base.getindex(a, ppkt.wr.wr_id[]+1)
 end
 
-# Index by `struct ibv_send_wr`
-function Base.getindex(a::AbstractArray, sendwr::c"struct ibv_send_wr")
-    Base.getindex(a, sendwr.wr_id+1)
+# Index by `Cptr{c"struct ibv_send_wr"}`
+function Base.getindex(a::AbstractArray, pwr::Cptr{c"struct ibv_send_wr"})
+    Base.getindex(a, pwr.wr_id[]+1)
 end
 
-# Index by `struct hashpipe_ibv_recv_pkt`
-function Base.getindex(a::AbstractArray, recvpkt::c"struct hashpipe_ibv_recv_pkt")
-    Base.getindex(a, recvpkt.wr.wr_id+1)
+# Index by `Cptr{c"struct hashpipe_ibv_recv_pkt"}`
+function Base.getindex(a::AbstractArray, ppkt::Cptr{c"struct hashpipe_ibv_recv_pkt"})
+    Base.getindex(a, ppkt.wr.wr_id[]+1)
 end
 
-# Index by `struct ibv_recv_wr`
-function Base.getindex(a::AbstractArray, recvwr::c"struct ibv_recv_wr")
-    Base.getindex(a, recvwr.wr_id+1)
+# Index by `Cptr{c"struct ibv_recv_wr"}`
+function Base.getindex(a::AbstractArray, pwr::Cptr{c"struct ibv_recv_wr"})
+    Base.getindex(a, pwr.wr_id[]+1)
 end
--- src/iterate.jl
+++ src/iterate.jl
@@ -1,62 +1,58 @@
 import Base: IteratorSize
 import Base: iterate
 
-# Iterate linked list of `struct hashpipe_ibv_recv_pkt`
-function Base.IteratorSize(::c"struct hashpipe_ibv_send_pkt")
+# Iterate linked list pointed to by `Cptr{c"struct hashpipe_ibv_send_pkt"}`
+function Base.IteratorSize(::Cptr{c"struct hashpipe_ibv_send_pkt"})
     Base.SizeUnknown()
 end
 
-function Base.iterate(pkt::c"struct hashpipe_ibv_send_pkt")
-  (pkt, Cptr{c"struct hashpipe_ibv_send_pkt"}(UInt(pkt.wr.next)))
+function Base.iterate(ppkt::Cptr{c"struct hashpipe_ibv_send_pkt"})
+  ppkt == C_NULL && return nothing
+  (ppkt, Cptr{c"struct hashpipe_ibv_send_pkt"}(UInt(ppkt.wr.next[])))
 end
 
-function Base.iterate(::c"struct hashpipe_ibv_send_pkt", next::Cptr{c"struct hashpipe_ibv_send_pkt"})
-  next == C_NULL && return nothing
-  pkt = next[]
-  (pkt, Cptr{c"struct hashpipe_ibv_send_pkt"}(UInt(pkt.wr.next)))
+function Base.iterate(::Cptr{c"struct hashpipe_ibv_send_pkt"}, next::Cptr{c"struct hashpipe_ibv_send_pkt"})
+  Base.iterate(next)
 end
 
-# Iterate linked list of `struct ibv_send_wr`
-function Base.IteratorSize(::c"struct ibv_send_wr")
+# Iterate linked list pointed to by `Cptr{c"struct ibv_send_wr"}`
+function Base.IteratorSize(::Cptr{c"struct ibv_send_wr"})
     Base.SizeUnknown()
 end
 
-function Base.iterate(wr::c"struct ibv_send_wr")
-  (wr, wr.next)
+function Base.iterate(pwr::Cptr{c"struct ibv_send_wr"})
+  pwr == C_NULL && return nothing
+  (pwr, pwr.next[])
 end
 
-function Base.iterate(::c"struct ibv_send_wr", next::Cptr{c"struct ibv_send_wr"})
-  next == C_NULL && return nothing
-  wr = next[]
-  (wr, wr.next)
+function Base.iterate(::Cptr{c"struct ibv_send_wr"}, next::Cptr{c"struct ibv_send_wr"})
+  Base.iterate(next)
 end
 
-# Iterate linked list of `struct hashpipe_ibv_recv_pkt`
-function Base.IteratorSize(::c"struct hashpipe_ibv_recv_pkt")
+# Iterate linked list pointed to by `Cptr{c"struct hashpipe_ibv_recv_pkt"}`
+function Base.IteratorSize(::Cptr{c"struct hashpipe_ibv_recv_pkt"})
     Base.SizeUnknown()
 end
 
-function Base.iterate(pkt::c"struct hashpipe_ibv_recv_pkt")
-  (pkt, Cptr{c"struct hashpipe_ibv_recv_pkt"}(UInt(pkt.wr.next)))
+function Base.iterate(ppkt::Cptr{c"struct hashpipe_ibv_recv_pkt"})
+  ppkt == C_NULL && return nothing
+  (ppkt, Cptr{c"struct hashpipe_ibv_recv_pkt"}(UInt(ppkt.wr.next[])))
 end
 
-function Base.iterate(::c"struct hashpipe_ibv_recv_pkt", next::Cptr{c"struct hashpipe_ibv_recv_pkt"})
-  next == C_NULL && return nothing
-  pkt = next[]
-  (pkt, Cptr{c"struct hashpipe_ibv_recv_pkt"}(UInt(pkt.wr.next)))
+function Base.iterate(::Cptr{c"struct hashpipe_ibv_recv_pkt"}, next::Cptr{c"struct hashpipe_ibv_recv_pkt"})
+  Base.iterate(next)
 end
 
-# Iterate linked list of `struct ibv_recv_wr`
-function Base.IteratorSize(::c"struct ibv_recv_wr")
+# Iterate linked list pointed to by `Cptr{c"struct ibv_recv_wr"}`
+function Base.IteratorSize(::Cptr{c"struct ibv_recv_wr"})
     Base.SizeUnknown()
 end
 
-function Base.iterate(wr::c"struct ibv_recv_wr")
-  (wr, wr.next)
+function Base.iterate(pwr::Cptr{c"struct ibv_recv_wr"})
+  pwr == C_NULL && return nothing
+  (pwr, pwr.next[])
 end
 
-function Base.iterate(::c"struct ibv_recv_wr", next::Cptr{c"struct ibv_recv_wr"})
-  next == C_NULL && return nothing
-  wr = next[]
-  (wr, wr.next)
+function Base.iterate(::Cptr{c"struct ibv_recv_wr"}, next::Cptr{c"struct ibv_recv_wr"})
+  Base.iterate(next)
 end
