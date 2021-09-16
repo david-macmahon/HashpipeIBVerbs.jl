23e75c4cc0e4a84105bdca74a7726b042c2e75a0
David MacMahon <davidm@astro.berkeley.edu>
2021-09-16 18:09:22 +0000

    Add ability to iterate all send/recv pkt buffers
    The existing iteration via a pointer to the head of a linked list only
    iterates through the linked list elements, which is not always
    guaranteed.  The current hashpipe_ibverbs convention of initializing all
    pkt buffers as one long linked list is not guaranteed to never change.
    This new iteration ability provides a convenient way to iterate through
    all pkt buffers.
    
    Iterating a tuple of `(pointer_to_ctx, pointer_to_pktbuf)` will iterate
    through all `pkt_buf`s of the type pointer to by the second/last tuple
    element.  In practice, this often looks like this:
    
        for pkt in (pctx, pctx.send_pkt_buf)
            @show pkt
        end
    
    The pattern for iterating all `recv_pkt_buf` objects is similar, but
    with `recv_pkt_buf` rather than `send_pkt_buf`.

--- src/iterate.jl
+++ src/iterate.jl
@@ -1,7 +1,53 @@
 import Base: IteratorSize
 import Base: iterate
+import Base: length
+
+# Iterate all send pkts of a (pointer to a) hashpipe_ibv_context
+
+"""
+Tuple type for iterating complete list of send pkts in a haspipe_ibv_context.
+Instances can be created via `(pctx, pctx.send_pkt_buf)`.
+"""
+# Not sure why the type for the first element (`pctx`) needs to have the extra
+# parentheses, but they seem to be required.
+SendPktIterator = Tuple{Cptr{Impl.var"(c\"struct hashpipe_ibv_context\")"},
+                        Cptr{Cptr{c"struct hashpipe_ibv_send_pkt"}}}
+
+# Iterate all `Cptr{c"struct hashpipe_ibv_send_pkt"}` contained in the first
+# element of `tpctx`.  `tcptx` can be given as `(pctx, pctx.send_pkt_buf)`.
+
+function Base.length(tpctx::SendPktIterator)
+    tpctx[1].send_pkt_num[]
+end
+
+function Base.iterate(tpctx::SendPktIterator, i::Int=1)
+  return (1 <= i <= length(tpctx)) ? (tpctx[1].send_pkt_buf[]+i-1, i+1) : nothing
+end
+
+# Iterate all recv pkts of a (pointer to a) hashpipe_ibv_context
+
+"""
+Tuple type for iterating complete list of recv pkts in a haspipe_ibv_context.
+Instances can be created via `(pctx, pctx.recv_pkt_buf)`.
+"""
+# Not sure why the type for the first element (`pctx`) needs to have the extra
+# parentheses, but they seem to be required.
+RecvPktIterator = Tuple{Cptr{Impl.var"(c\"struct hashpipe_ibv_context\")"},
+                        Cptr{Cptr{c"struct hashpipe_ibv_recv_pkt"}}}
+
+# Iterate all `Cptr{c"struct hashpipe_ibv_recv_pkt"}` contained in the first
+# element of `tpctx`.  `tcptx` can be given as `(pctx, pctx.recv_pkt_buf)`.
+
+function Base.length(tpctx::RecvPktIterator)
+    tpctx[1].recv_pkt_num[]
+end
+
+function Base.iterate(tpctx::RecvPktIterator, i::Int=1)
+  return (1 <= i <= length(tpctx)) ? (tpctx[1].recv_pkt_buf[]+i-1, i+1) : nothing
+end
 
 # Iterate linked list pointed to by `Cptr{c"struct hashpipe_ibv_send_pkt"}`
+
 function Base.IteratorSize(::Cptr{c"struct hashpipe_ibv_send_pkt"})
     Base.SizeUnknown()
 end
@@ -16,6 +62,7 @@ function Base.iterate(::Cptr{c"struct hashpipe_ibv_send_pkt"}, next::Cptr{c"stru
 end
 
 # Iterate linked list pointed to by `Cptr{c"struct ibv_send_wr"}`
+
 function Base.IteratorSize(::Cptr{c"struct ibv_send_wr"})
     Base.SizeUnknown()
 end
@@ -30,6 +77,7 @@ function Base.iterate(::Cptr{c"struct ibv_send_wr"}, next::Cptr{c"struct ibv_sen
 end
 
 # Iterate linked list pointed to by `Cptr{c"struct hashpipe_ibv_recv_pkt"}`
+
 function Base.IteratorSize(::Cptr{c"struct hashpipe_ibv_recv_pkt"})
     Base.SizeUnknown()
 end
@@ -44,6 +92,7 @@ function Base.iterate(::Cptr{c"struct hashpipe_ibv_recv_pkt"}, next::Cptr{c"stru
 end
 
 # Iterate linked list pointed to by `Cptr{c"struct ibv_recv_wr"}`
+
 function Base.IteratorSize(::Cptr{c"struct ibv_recv_wr"})
     Base.SizeUnknown()
 end
